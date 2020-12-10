52caa815423c42bd6eacb75c7df5121a631a6dea
김동률 <ruylruyl@naver.com>
2020-12-10 16:12:29 +0900

    changing front EC2 url

--- PTJ/front_1/src/ApiService.js
+++ PTJ/front_1/src/ApiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const USER_API_BASE_URL = "http://localhost:8080/users";
-const USER_API_BASE_URL = "http://52.79.199.40:8080/users"; // EC2 프론트 : http://52.79.199.40:80
+const USER_API_BASE_URL = "http://15.165.238.196:8080/users"; // EC2 탄력적 IP (고정 IP)
 
 class ApiService{
 
